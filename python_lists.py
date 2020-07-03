21ac18e00c519fc3379f58029ccd303f83a4dad7
TeagenMcEwan <67173830+TeagenMcEwan@users.noreply.github.com>
2020-07-03 15:44:16 +0800

    Exercise responses for Q1 to Q3 for Lists..

--- python_lists.py
+++ python_lists.py
@@ -1,9 +1,41 @@
-print("fruit yay!")
+# Question 1
 
-foods = ["orange", "apple", "banana", "strawberry", "grape", "blueberry", "carrot", "cauliflower", "pumpkin", "passionfruit", "mango", "kiwifruit"]
+# print("fruit yay!")
 
-print(foods)
+# foods = ["orange", "apple", "banana", "strawberry", "grape", "blueberry", "carrot", "cauliflower", "pumpkin", "passionfruit", "mango", "kiwifruit"]
+
+# print(foods)
+
+# print(foods[0])
+# print(foods[2])
+# print(foods[11])
+# print(foods[0:3])
+# print(foods[-3:])
+# print(foods[-4])
 
 
+# Question 2
 
+# mailing_list = [
+#     ["Roary", "roary@moth.catchers"],
+#     ["Remus", "remus@kapers.dog"],
+#     ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+#     ["Biscuit", "biscuit@whippies.park"],
+#     ["Rory", "rory@whippies.park"]
+# ]
+
+# for item in mailing_list:
+#     print(f"{item[0]:<5}: {item[1]}")
+
+
+# Question 3
+
+# name = input("List three names?")
+# print(name)
+
+# names = ["Izzy", "Archie", "Boston"]
+# print(names)
+
+
+# Question 4
 
