cfccac0fcfd2bbe6ddacc7d314be252a378c16a8
TeagenMcEwan <67173830+TeagenMcEwan@users.noreply.github.com>
2020-07-10 16:10:59 +0800

    Final review of Lists and Tweaking on Loops

--- python_lists.py
+++ python_lists.py
@@ -1,51 +1,51 @@
-# Question 1
+# # Question 1
 
-print("fruit yay!")
+# print("fruit yay!")
 
-foods = ["orange", "apple", "banana", "strawberry", "grape", "blueberry", "carrot", "cauliflower", "pumpkin", "passionfruit", "mango", "kiwifruit"]
+# foods = ["orange", "apple", "banana", "strawberry", "grape", "blueberry", "carrot", "cauliflower", "pumpkin", "passionfruit", "mango", "kiwifruit"]
 
-print(foods)
+# print(foods)
 
-print(foods[0])
-print(foods[2])
-print(foods[11])
-print(foods[0:3])
-print(foods[-3:])
-print(foods[-4])
-
-
-# Question 2
-
-mailing_list = [
-    ["Roary", "roary@moth.catchers"],
-    ["Remus", "remus@kapers.dog"],
-    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
-    ["Biscuit", "biscuit@whippies.park"],
-    ["Rory", "rory@whippies.park"]
-]
-
-for item in mailing_list:
-    print(f"{item[0]:<5}: {item[1]}")
+# print(foods[0])
+# print(foods[2])
+# print(foods[11])
+# print(foods[0:3])
+# print(foods[-3:])
+# print(foods[-4])
 
 
-# Question 3
+# # Question 2
 
-names = ["Izzy", "Archie", "Boston"]
+# mailing_list = [
+#     ["Roary", "roary@moth.catchers"],
+#     ["Remus", "remus@kapers.dog"],
+#     ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+#     ["Biscuit", "biscuit@whippies.park"],
+#     ["Rory", "rory@whippies.park"]
+# ]
 
-names = input("List three names?")
-print(names)
+# for item in mailing_list:
+#     print(f"{item[0]:<5}: {item[1]}")
 
-names = ["Izzy", "Archie", "Boston"]
-print(names)
+
+# # Question 3
+
+# names = ["Izzy", "Archie", "Boston"]
+
+# names = input("List three names?")
+# print(names)
+
+# names = ["Izzy", "Archie", "Boston"]
+# print(names)
 
 
 # Question 4
 
-# item = "this is a string" 
-# print(item)
+item = "this is a string" 
+print(len(str.split(item)))
+print(str.split(item))
 
-# day = "what a lovely day!" 
-# print(day)
 
-# for item in range("a"0, 50, 5):
-#     print(index)
+day = "what a lovely day!" 
+print(len(str.split(day)))
+print(str.split(day))
--- python_loops.py
+++ python_loops.py
@@ -1,12 +1,54 @@
-#while loops
+# Loops - For Loops and While Loops
+
+# Question 1
 
 counter = 8
 
 while counter > 0:
     print(counter)
 
-    # if counter%2 == 0:
-    #     print(f"{counter} is an even number.")
-    # else:
-    #     print(f"{counter} is an odd number.")
-    # counter = counter - 1
+    if counter%2 == 0:
+        print(f"{counter} is an even number.")
+    else:
+        print(f"{counter} is an odd number.")
+
+
+# Question 2
+
+mailing_list = [
+    ["Roary", "roary@moth.catchers"],
+    ["Remus", "remus@kapers.dog"],
+    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+    ["Biscuit", "biscuit@whippies.park"],
+    ["Rory", "rory@whippies.park"]
+]
+
+# print(mailing_list)
+
+for item in mailing_list: 
+    print(f"{item[0]:<5}: {item[1]}")
+
+# Question 3
+
+names = ["Izzy", "Archie", "Boston"]
+
+counter = 0
+while counter < 3:
+    names = input("What is your name? ")
+    counter = counter + 1
+
+print(names)
+
+
+# # Question 4
+
+groceries = [
+    ["Baby Spinach", 2.78],
+    ["Hot Chocolate", 3.70],
+    ["Crackers", 2.10],
+    ["Bacon", 9.00],
+    ["Carrots", 0.56],
+    ["Oranges", 3.08]
+]
+for item in groceries:
+    print(f"{item[0]:<20} ${item[1]:.2f}")
