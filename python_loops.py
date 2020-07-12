ae7392075d702ee4cb5b3205be88766004f2fd66
TeagenMcEwan <67173830+TeagenMcEwan@users.noreply.github.com>
2020-07-12 15:39:05 +0800

    Changes to Loops and Read Write files

--- python_loops.py
+++ python_loops.py
@@ -2,59 +2,73 @@
 
 # Question 1
 
+output_numbered_list = False
+# number = input("Enter a number:")
+
+number_list = []
+while output_numbered_list == False:
+    number = input("Enter a number:")
+    if number == "":
+        output_numbered_list = True 
+    else:
+        number_list.append(int(number))
+print(number_list)
+total = sum(number_list)
+print(total)
+
 
 
 # Question 2
 
-mailing_list = [
-    ["Roary", "roary@moth.catchers"],
-    ["Remus", "remus@kapers.dog"],
-    ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
-    ["Biscuit", "biscuit@whippies.park"],
-    ["Rory", "rory@whippies.park"]
-]
+# mailing_list = [
+#     ["Roary", "roary@moth.catchers"],
+#     ["Remus", "remus@kapers.dog"],
+#     ["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+#     ["Biscuit", "biscuit@whippies.park"],
+#     ["Rory", "rory@whippies.park"]
+# ]
 
-print(mailing_list)
+# print(mailing_list)
 
-for item in mailing_list: 
-    print(f"{item[0]:<5}: {item[1]}")
+# for item in mailing_list: 
+#     print(f"{item[0]:<5}: {item[1]}")
 
 
 
 # Question 3
 
-names = ["Izzy", "Archie", "Boston"]
+# names = ["Izzy", "Archie", "Boston"]
 
-counter = 0
-while counter < 3:
-    names = input("What is your name? ")
-    counter = counter + 1
+# counter = 0
+# while counter < 3:
+#     names = input("What is your name? ")
+#     counter = counter + 1
 
-print(names)
+# print(names)
 
 
 
 # # Question 4
 
-groceries = [
-    ["Baby Spinach", 2.78],
-    ["Hot Chocolate", 3.70],
-    ["Crackers", 2.10],
-    ["Bacon", 9.00],
-    ["Carrots", 0.56],
-    ["Oranges", 3.08]
-]
-total = 0
+# groceries = [
+#     ["Baby Spinach", 2.78],
+#     ["Hot Chocolate", 3.70],
+#     ["Crackers", 2.10],
+#     ["Bacon", 9.00],
+#     ["Carrots", 0.56],
+#     ["Oranges", 3.08]
+# ]
+# total = 0
 
-for item in groceries:
-    quantity = input(f"What quantity of {item[0]} would you like?")
-    item[1] = item[1] *int(quantity)
-    total += item[1]
+# for item in groceries:
+#     quantity = input(f"What quantity of {item[0]} would you like?")
+#     item[1] = item[1] *int(quantity)
+#     total += item[1]
 
-total = f"${total:.2f}"
+# total = f"${total:.2f}"
 
-print("==== Izzy's Food Emporium ====")
-for item in groceries:
-    print(f"{item[0]:<20} ${item[1]:.2f}")
-print("==============================")
-print(f"{total:>27}")
+# print("==== Izzy's Food Emporium ====")
+# for item in groceries:
+#     print(f"{item[0]:<20} ${item[1]:.2f}")
+# print("==============================")
+# print(f"{total:>27}")
---
+++ python_readwrite.py
@@ -0,0 +1,8 @@
+# Reading and Writing Files
+
+# Question 1
+
+names = []
+
+with open("names.txt") as txt_file:
+    for line in txt_file:
