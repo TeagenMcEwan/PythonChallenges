f775ad0d2794fb07d9314eefcc42590dc1b50737
TeagenMcEwan <67173830+TeagenMcEwan@users.noreply.github.com>
2020-07-20 06:11:30 +0800

    work in progress changes

--- python_loops.py
+++ python_loops.py
@@ -2,19 +2,19 @@
 
 # Question 1
 
-output_numbered_list = False
-# number = input("Enter a number:")
+# output_numbered_list = False
+# # number = input("Enter a number:")
 
-number_list = []
-while output_numbered_list == False:
-    number = input("Enter a number:")
-    if number == "":
-        output_numbered_list = True 
-    else:
-        number_list.append(int(number))
-print(number_list)
-total = sum(number_list)
-print(total)
+# number_list = []
+# while output_numbered_list == False:
+#     number = input("Enter a number:")
+#     if number == "":
+#         output_numbered_list = True 
+#     else:
+#         number_list.append(int(number))
+# print(number_list)
+# total = sum(number_list)
+# print(total)
 
 
 
@@ -50,25 +50,25 @@ print(total)
 
 # # Question 4
 
-# groceries = [
-#     ["Baby Spinach", 2.78],
-#     ["Hot Chocolate", 3.70],
-#     ["Crackers", 2.10],
-#     ["Bacon", 9.00],
-#     ["Carrots", 0.56],
-#     ["Oranges", 3.08]
-# ]
-# total = 0
+groceries = [
+    ["Baby Spinach", 2.78],
+    ["Hot Chocolate", 3.70],
+    ["Crackers", 2.10],
+    ["Bacon", 9.00],
+    ["Carrots", 0.56],
+    ["Oranges", 3.08]
+]
+total = 0
 
-# for item in groceries:
-#     quantity = input(f"What quantity of {item[0]} would you like?")
-#     item[1] = item[1] *int(quantity)
-#     total += item[1]
+for item in groceries:
+    quantity = input(f"What quantity of {item[0]} would you like?")
+    item[1] = item[1] *int(quantity)
+    total += item[1]
 
-# total = f"${total:.2f}"
+total = f"${total:.2f}"
 
-# print("==== Izzy's Food Emporium ====")
-# for item in groceries:
-#     print(f"{item[0]:<20} ${item[1]:.2f}")
-# print("==============================")
-# print(f"{total:>27}")
+print("==== Izzy's Food Emporium ====")
+for item in groceries:
+    print(f"{item[0]:<20} ${item[1]:.2f}")
+print("==============================")
+print(f"{total:>27}")
--- python_readwrite.py
+++ python_readwrite.py
@@ -6,3 +6,25 @@ names = []
 
 with open("names.txt") as txt_file:
     for line in txt_file:
+
+
+
+# Question 2
+
+import csv
+
+colour_list = []
+english_name_list = []
+
+with open("colours_20.csv") as csv_file:
+    reader = csv.reader(csv_file)
+    for line in reader:
+        colour_list.append(line)
+
+# for colour_name in colour_list:
+#    print(colour_name[4])
+
+for (colour_name) in (colour_list):
+    english_name_list.append(colour_name)
+    
+print(english_name_list)
